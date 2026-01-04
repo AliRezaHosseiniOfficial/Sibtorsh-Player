e4df6bb4ca062256221685664a860008695d3889
Alireza Hosseini <alireza.hosseini7902@gmail.com>
2026-01-04 15:11:26 +0330

    - Bug fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export {default} from './VideoPlayer';
 export {default as VideoPlayer} from './VideoPlayer';
 export type {default as VideoPlayerInterface} from './interfaces/VideoPlayerInterface';
 export {default as VideoPlayerControl} from './components/VideoPlayerControl';
